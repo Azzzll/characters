8400bc160ac952543f9d214bf52e086ac8b25373
makesomelayouts <makesomelayouts@gmail.com>
2025-01-24 16:42:29 +0400

    Init

---
+++ male-character/scripts/main.js
@@ -0,0 +1,4 @@
+// imports
+
+import "./appearence-sidebar.js";
+import "./character-sidebar.js";
---
+++ scripts/characters.js
@@ -0,0 +1,6 @@
+// TODO: add characters:
+// if we have saved one from storage then load him else load default state
+// if (savedMenCharacter.innerHTML) { menCharacter.innerHTML = savedMenCharacter.innerHTML } else { default }
+
+// else =
+const manCharacter = document.createElement("div");
